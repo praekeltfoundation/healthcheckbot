a0bcb2ed4dea0c9f6bca1266e6533254875e59a0
Milton Madanda <madandat@gmail.com>
2020-06-12 18:13:19 +0200

    add risk assessment

---
+++ actions/utils.py
@@ -0,0 +1,28 @@
+from typing import Any, Dict, Text
+
+
+def get_risk_level(data: Dict[Text, Any]) -> Text:
+    symptoms = 0
+    for key, value in data.items():
+        if "symptoms_" in key and value == "yes":
+            symptoms += 1
+
+    if symptoms >= 3:
+        return "high"
+    elif symptoms == 2:
+        if data["exposure"] == "yes" or data["age"] == ">65":
+            return "high"
+        else:
+            return "moderate"
+    elif symptoms == 1:
+        if data["exposure"] == "yes":
+            return "high"
+        else:
+            return "moderate"
+    elif symptoms == 0:
+        if data["exposure"] == "yes":
+            return "moderate"
+        else:
+            return "low"
+
+    return "not_sure"
