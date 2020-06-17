889e79ec00289266f6de6f372f7cd0c780ae09ab
Milton Madanda <madandat@gmail.com>
2020-06-17 09:11:37 +0200

    fix what black broke

--- actions/actions.py
+++ actions/actions.py
@@ -201,8 +201,8 @@ class HealthCheckProfileForm(BaseFormAction):
 
         # Use location pin data if submitted
         if metadata and metadata.get("type") == "location":
-            latitude = (tracker.metadata["location"]["latitude"],)
-            longitude = (tracker.metadata["location"]["longitude"],)
+            latitude = tracker.metadata["location"]["latitude"]
+            longitude = tracker.metadata["location"]["longitude"]
             address = tracker.metadata["location"].get("address")
             if not address:
                 address = f"GPS: {latitude}, {longitude}"
