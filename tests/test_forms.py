83d380e94a987e249789ed217358e0fe4b9b5913
Milton Madanda <madandat@gmail.com>
2020-07-03 10:42:02 +0200

    add tests for terms

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -5,42 +5,49 @@ from rasa_sdk.events import SlotSet, Form
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
 
-from actions.actions import HealthCheckProfileForm
+from actions.actions import HealthCheckProfileForm, HealthCheckTermsForm
+from tests import utils
 
 
 class TestHealthCheckProfileForm:
-    def get_tracker_for_number_slot_with_value(self, slot_name, value):
-        return Tracker(
-            "default",
-            {"requested_slot": slot_name},
-            {"entities": [{"entity": "number", "value": value}]},
-            [],
-            False,
-            None,
-            {},
-            "action_listen",
-        )
-
     @pytest.mark.asyncio
     async def test_validate_age(self):
         form = HealthCheckProfileForm()
 
-        tracker = self.get_tracker_for_number_slot_with_value("age", "1")
+        tracker = utils.get_tracker_for_number_slot_with_value(form, "age", "1")
         events = await form.validate(CollectingDispatcher(), tracker, {})
         assert events == [SlotSet("age", "<18")]
 
-        tracker = self.get_tracker_for_number_slot_with_value("age", "2")
+        tracker = utils.get_tracker_for_number_slot_with_value(form, "age", "2")
         events = await form.validate(CollectingDispatcher(), tracker, {})
         assert events == [SlotSet("age", "18-39")]
 
-        tracker = self.get_tracker_for_number_slot_with_value("age", "3")
+        tracker = utils.get_tracker_for_number_slot_with_value(form, "age", "3")
         events = await form.validate(CollectingDispatcher(), tracker, {})
         assert events == [SlotSet("age", "40-65")]
 
-        tracker = self.get_tracker_for_number_slot_with_value("age", "4")
+        tracker = utils.get_tracker_for_number_slot_with_value(form, "age", "4")
         events = await form.validate(CollectingDispatcher(), tracker, {})
         assert events == [SlotSet("age", ">65")]
 
-        tracker = self.get_tracker_for_number_slot_with_value("age", ["2", "39"])
+        tracker = utils.get_tracker_for_number_slot_with_value(form, "age", ["2", "39"])
         events = await form.validate(CollectingDispatcher(), tracker, {})
         assert events == [SlotSet("age", None)]
+
+        tracker = utils.get_tracker_for_number_slot_with_value(form, "age", "not a number")
+        events = await form.validate(CollectingDispatcher(), tracker, {})
+        assert events == [SlotSet("age", None)]
+
+
+class TestHealthCheckTermsForm:
+    @pytest.mark.asyncio
+    async def test_validate_terms(self):
+        form = HealthCheckTermsForm()
+
+        tracker = utils.get_tracker_for_slot_from_intent(form, "terms", "affirm")
+        events = await form.validate(CollectingDispatcher(), tracker, {})
+        assert events == [SlotSet("terms", "yes")]
+
+        tracker = utils.get_tracker_for_slot_from_intent(form, "terms", "more")
+        events = await form.validate(CollectingDispatcher(), tracker, {})
+        assert events == [SlotSet("terms", None)]
