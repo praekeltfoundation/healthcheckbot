aad9274a1f39cd1d8ae9bf2422ba08ad590c0e44
Rudi <rudi@praekelt.org>
2020-07-10 15:19:44 +0200

    Create a DBE bot, by cloning the existing bot (#30)
    * Add DBE bot, copy/symlink of base
    
    * Add actions to dbe actions
    
    * Make dbe actions a module
    
    * Add tests directory
    
    * Link over conversation tests

---
+++ dbe/actions/actions.py
@@ -0,0 +1,15 @@
+from base.actions.actions import (
+    ActionExit,
+    ActionSessionStart,
+    HealthCheckForm,
+    HealthCheckProfileForm,
+    HealthCheckTermsForm,
+)
+
+__all__ = [
+    "HealthCheckTermsForm",
+    "HealthCheckProfileForm",
+    "HealthCheckForm",
+    "ActionSessionStart",
+    "ActionExit",
+]
