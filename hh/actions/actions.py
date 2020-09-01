46815764b910989b46a735290c60b363cea325a6
Rudi Giesler <rudi@praekelt.org>
2020-09-01 15:50:58 +0200

    Add first and last name

--- hh/actions/actions.py
+++ hh/actions/actions.py
@@ -1,10 +1,60 @@
+from typing import Any, Dict, List, Optional, Text, Union
+
+from rasa_sdk import Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import Action
+
 from base.actions.actions import (
-    ActionExit,
-    ActionSessionStart,
-    HealthCheckForm,
-    HealthCheckProfileForm,
+    ActionExit as BaseActionExit,
+    ActionSessionStart as BaseActionSessionStart,
+    HealthCheckForm as BaseHealthCheckForm,
+    HealthCheckProfileForm as BaseHealthCheckProfileForm,
     HealthCheckTermsForm,
 )
+
+class HealthCheckProfileForm(BaseHealthCheckProfileForm):
+    PERSISTED_SLOTS = [
+        "first_name",
+        "last_name",
+        "gender",
+        "province",
+        "location",
+        "location_confirm",
+        "medical_condition",
+    ]
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+        mappings = super().slot_mappings()
+        mappings["first_name"] = [self.from_text()]
+        mappings["last_name"] = [self.from_text()]
+        return mappings
+
+
+class HealthCheckForm(BaseHealthCheckForm):
+    def get_eventstore_data(self, tracker: Tracker, risk: Text) -> Dict[Text, Any]:
+        data = super().get_eventstore_data(tracker, risk)
+        data["first_name"] = tracker.get_slot("first_name")
+        data["last_name"] = tracker.get_slot("last_name")
+        return data
+
+class ActionSessionStart(BaseActionSessionStart):
+    def get_carry_over_slots(self, tracker: Tracker) -> List[Dict[Text, Any]]:
+        actions = super().get_carry_over_slots(tracker)
+        carry_over_slots = ("first_name", "last_name")
+        for slot in carry_over_slots:
+            actions.append(SlotSet(slot, tracker.get_slot(slot)))
+        return actions
+
+class ActionExit(BaseActionExit):
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_exit")
+        return ActionSessionStart().get_carry_over_slots(tracker)
 
 __all__ = [
     "HealthCheckTermsForm",
@@ -12,5 +62,4 @@ __all__ = [
     "HealthCheckForm",
     "ActionSessionStart",
     "ActionExit",
-    "ActionSessionStart",
 ]
---
+++ hh/tests/test_actions.py
@@ -0,0 +1,144 @@
+from unittest import TestCase
+
+from rasa_sdk import Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+
+from hh.actions.actions import (
+    ActionExit,
+    ActionSessionStart,
+    HealthCheckForm,
+    HealthCheckProfileForm,
+)
+
+class HealthCheckProfileFormTests(TestCase):
+    def test_slot_mappings(self):
+        """
+        Ensures that the additional school fields are in the slot mappings
+        """
+        form = HealthCheckProfileForm()
+        mappings = form.slot_mappings()
+        self.assertIn("first_name", mappings)
+        self.assertIn("last_name", mappings)
+
+
+class HealthCheckFormTests(TestCase):
+    def test_eventstore_data(self):
+        """
+        The data is transformed from the tracker store into the event store format
+        """
+        form = HealthCheckForm()
+        tracker = Tracker(
+            "27820001001",
+            {
+                "province": "wc",
+                "first_name": "test first",
+                "last_name": "test last",
+                "age": "40-65",
+                "symptoms_fever": "no",
+                "symptoms_cough": "yes",
+                "symptoms_sore_throat": "no",
+                "symptoms_difficulty_breathing": "no",
+                "symptoms_taste_smell": "no",
+                "medical_condition": "not sure",
+                "exposure": "not sure",
+                "tracing": "yes",
+                "gender": "OTHER",
+                "location": "Long Street, Cape Town",
+                "medical_condition_obesity": "no",
+                "medical_condition_diabetes": "no",
+                "medical_condition_hypertension": "yes",
+                "medical_condition_cardio": "no",
+            },
+            {},
+            [],
+            False,
+            None,
+            {},
+            "action_listen",
+        )
+        data = form.get_eventstore_data(tracker, "low")
+        self.assertTrue(data.pop("deduplication_id"))
+        self.assertEqual(
+            data,
+            {
+                "first_name": "test first",
+                "last_name": "test last",
+                "province": "ZA-WC",
+                "age": "40-65",
+                "fever": False,
+                "cough": True,
+                "sore_throat": False,
+                "difficulty_breathing": False,
+                "smell": False,
+                "preexisting_condition": "not_sure",
+                "exposure": "not_sure",
+                "tracing": True,
+                "gender": "other",
+                "city": "Long Street, Cape Town",
+                "city_location": "",
+                "location": "",
+                "msisdn": "+27820001001",
+                "risk": "low",
+                "source": "WhatsApp",
+                "data": {
+                    "cardio": False,
+                    "diabetes": False,
+                    "hypertension": True,
+                    "obesity": False,
+                },
+            },
+        )
+
+
+class ActionSessionStartTests(TestCase):
+    def test_name_details_copied(self):
+        """
+        Should copy over the name details to the new session
+        """
+        action = ActionSessionStart()
+        events = action.get_carry_over_slots(
+            Tracker(
+                "27820001001",
+                {
+                    "first_name": "test first",
+                    "last_name": "test last",
+                },
+                {},
+                [],
+                False,
+                None,
+                {},
+                "action_listen",
+            )
+        )
+        self.assertIn(SlotSet("first_name", "test first"), events)
+        self.assertIn(SlotSet("last_name", "test last"), events)
+
+
+class ActionExitTests(TestCase):
+    def test_name_details_copied(self):
+        """
+        Should copy over the name details when exiting
+        """
+        action = ActionExit()
+        dispatcher = CollectingDispatcher()
+        events = action.run(
+            dispatcher,
+            Tracker(
+                "27820001001",
+                {
+                    "first_name": "test first",
+                    "last_name": "test last",
+                },
+                {},
+                [],
+                False,
+                None,
+                {},
+                "action_listen",
+            ),
+            {}
+        )
+        self.assertIn(SlotSet("first_name", "test first"), events)
+        self.assertIn(SlotSet("last_name", "test last"), events)
