6def2fa50226af58aa25f7659f5cc6f38e507d7b
Codie Roelf <codiebeulaine@gmail.com>
2021-04-21 23:43:50 +0200

    add names to test tracker

--- hh/tests/test_actions.py
+++ hh/tests/test_actions.py
@@ -207,7 +207,10 @@ class HealthCheckFormTests(TestCase):
         dt.now.return_value = datetime(
             2020, 1, 2, 3, 4, 5, tzinfo=timezone(timedelta(hours=2))
         )
-        tracker = Tracker("27820001001", {}, {}, [], False, None, {}, "action_listen",)
+        tracker = Tracker("27820001001", {
+            "first_name": "test first",
+            "last_name": "test last"
+        },{}, {}, [], False, None, {}, "action_listen",)
         form.send_risk_to_user(dispatcher, "low", tracker)
         [msg] = dispatcher.messages
         self.assertEqual(msg["template"], "utter_risk_low")
