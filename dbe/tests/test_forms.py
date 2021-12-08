92bc11624335c809e297c8af57cd01194dd553ae
Codie Roelf <codiebeulaine@gmail.com>
2021-12-08 12:06:17 +0200

    update consent dbe

--- dbe/tests/test_forms.py
+++ dbe/tests/test_forms.py
@@ -7,7 +7,37 @@ from rasa_sdk.executor import CollectingDispatcher
 
 import base.actions.actions
 from base.tests import utils
-from dbe.actions.actions import HealthCheckForm, HealthCheckProfileForm
+from dbe.actions.actions import (
+    HealthCheckForm,
+    HealthCheckProfileForm,
+    DBEHealthCheckTermsForm,
+)
+from rasa_sdk.events import SlotSet, Form
+
+
+class TestDBEHealthCheckTermsForm:
+    @pytest.mark.asyncio
+    async def test_validate_terms(self):
+        form = DBEHealthCheckTermsForm()
+        dispatcher = CollectingDispatcher()
+
+        tracker = utils.get_tracker_for_slot_from_intent(form, "terms", "affirm")
+        events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
+        assert form.required_slots(tracker) == ["terms"]
+        assert events == [
+            SlotSet("terms", "yes"),
+            Form(None),
+            SlotSet("requested_slot", None),
+        ]
+
+        tracker = utils.get_tracker_for_slot_from_intent(form, "terms", "deny")
+        events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
+        assert form.required_slots(tracker) == ["terms"]
+        assert events == [SlotSet("terms", "")]
+
+        tracker = utils.get_tracker_for_slot_from_intent(form, "terms", "more")
+        events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
+        assert events == [SlotSet("terms", None), SlotSet("requested_slot", "terms")]
 
 
 class TestHealthCheckForm:
